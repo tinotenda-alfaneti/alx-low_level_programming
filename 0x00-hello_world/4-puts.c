01b8733aca6d8fa51c82598e04a51a5ab703b6c0
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-08 18:31:19 +0000

    Writng a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /*Function that puts a text to the console*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
