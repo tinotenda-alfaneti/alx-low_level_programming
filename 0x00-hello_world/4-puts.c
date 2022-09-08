b60e292085c25760ce61b714f2833b014bc1739b
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-08 18:56:52 +0000

    Writng a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/*Function that puts a text to the console*/
+/**
+ *main - Short description, single line
+ *
+ *Description: Longer description of the function
+ *section header: Section description
+ *Return: 0 Description of the returned value
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
