bf13c77a683e34581f3e316c1de824b61cbb77af
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-08 19:15:29 +0000

    Using printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Short description, single line
+ *
+ *Description: Longer description of the function
+ *section header: Section description
+ *Return: 0 Description of the returned value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
