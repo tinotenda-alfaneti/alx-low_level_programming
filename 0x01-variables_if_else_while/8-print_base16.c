3feac8ed98d9f68911d39b88f35db4de99cf0b53
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-10 01:32:30 +0000

    Print the number line in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers in hexadecimal format
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	int x;
+
+	for (i = 0; i <= 9; ++i)
+		putchar(i + '0');
+
+	for (i = 'a'; i <= 'f'; ++i)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
