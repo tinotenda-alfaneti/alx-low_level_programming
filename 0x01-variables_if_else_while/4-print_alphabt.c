3a7da5fe6cd6157539bb792481f2c712cc77cb80
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-10 00:49:14 +0000

    Alphabet without other letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/*
+ *File: 4-print_alphabt.c
+ *Auth: Tinotenda Rodney Alfaneti
+ */
+#include <stdio.h>
+/**
+ * main - Prints the whole alphabet except the q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+		if (letter != 'q' || letter != 'e')
+			putchar(letter);
+	}
+	printf('\n');
+
+	return (0);
+}
