8a8d3f578d4d0e815277381d9c972a308a38d3b2
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-10 00:38:21 +0000

    Lowecase and uppercase of the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/*
+ *File: 3-print_alphabets.c
+ *Auth: Tinotenda Rodney Alfaneti
+ */
+
+#include <stdio.h>
+
+/**
+ *main - Prints the alphabet, lower and upper case
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; ++c)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
