64d254dd05365b381187ed37d32d4799a0b21396
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-10 01:11:23 +0000

    Add numbers together

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print numbers without using a char variable
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i <= 9; ++i)
+		putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
