f78ced7a8ea8d508c429140204cac51901d68bc5
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-10 01:47:43 +0000

    Print the combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints the combinations of numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
