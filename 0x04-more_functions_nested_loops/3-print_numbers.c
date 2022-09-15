5e9f32e917b2c55a3e126f04418c5b4fb514f138
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-15 20:19:52 +0000

    Print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: always nothing
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+
+	_putchar('\n');
+
+}
