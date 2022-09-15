b8de2658b608aa7a9ba463927cef35c08d0d90b4
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-15 20:04:50 +0000

    Multiply integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the product of the integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
