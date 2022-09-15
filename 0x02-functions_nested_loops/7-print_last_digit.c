f636b54c5c002b6dc60a6d704e457600297928cc
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-15 13:57:54 +0000

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit of a number
+ * @i: The integer from which the last digit is going to be found
+ *
+ * Return: The last digit of the number given
+ */
+int print_last_digit(int i)
+{
+	int las = i % 10;
+
+	if (i < 0)
+	{
+		return (-1 * las);
+	}
+
+	return (las);
+}
