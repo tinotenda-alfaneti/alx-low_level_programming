bf21ad30ddf6a582aaf26a4b35a9b8d90d084971
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-15 12:45:44 +0000

    Print 10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Printing the alphabet 10 times
+ *
+ * Return: Always returns zero
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+
+		i++;
+	}
+
+	return (0);
+
+}
