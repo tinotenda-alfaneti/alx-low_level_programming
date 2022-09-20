349e7e3a3572c780bed7c3ca348f9d52ae072540
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-20 18:54:26 +0000

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: string to count
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		i++;
+	}
+
+	return (c);
+}
