3102a66be9613c70c3dde9a5ba91d2613ada296f
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-20 21:03:24 +0000

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - coppying a string
+ * @dest: value destination
+ * @src: value of source
+ *
+ * Return: pointer to destination\
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = str[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
