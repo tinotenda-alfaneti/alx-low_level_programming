4464b62ca38c3ea4eb4d9df3172ff89481dceca3
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-20 20:18:32 +0000

    Print half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half the string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int j = _strlen(*str), i;
+
+	if (j % 2 == 0)
+	{
+		i = j / 2;
+	}
+	else
+	{
+		i = (j - 1) / 2;
+	}
+
+	for (; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
