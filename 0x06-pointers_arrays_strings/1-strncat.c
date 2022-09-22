a07bcf822e2b770701923be79ed11b22a3291c6f
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-22 19:25:28 +0000

    Concatenate strings with n letters from string src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - A function to concatenate strings
+ * @dest: The pointer to the final location of the string
+ * @src: the source of the other part of the string
+ * @n: The number of characters to be taken
+ *
+ * Return: A pointer to the result string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = 0, i = 0;
+
+	while (dest[d])
+	{
+		d++;
+	}
+
+	for (; i < n; i++)
+	{
+		dest[d] = src[i];
+		d++;
+	}
+
+	dest[d] = '\0';
+	return (dest);
+}
