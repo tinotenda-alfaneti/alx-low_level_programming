917a31a56e328923d4dc37f2e1495b059302fbfd
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-27 17:23:38 +0000

    Change value from a pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of pointer to char
+ * @s: the pointer
+ * @to: the character
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
