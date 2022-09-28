0a0a6e3e19cf63acb270996b7517e8f429a6bf50
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-28 22:38:15 +0000

    Factorial using recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - calculate the factorial of number
+ * @n: the number
+ *
+ * Return: the factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
