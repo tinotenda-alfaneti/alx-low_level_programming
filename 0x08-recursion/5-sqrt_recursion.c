99dd26789a94606f43631a4da9d632e769f49ba8
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-09-29 00:35:16 +0000

    Calculate message recursively

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculates square root
+ * @n: the number
+ *
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (_helper(1, n));
+}
+
+/**
+ * helper - checks for squareroot
+ * @a: guess number
+ * @b: number to find square root for
+ *
+ * Return: square root or -1
+ */
+
+int _helper(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (_helper(a + 1, b));
+}
