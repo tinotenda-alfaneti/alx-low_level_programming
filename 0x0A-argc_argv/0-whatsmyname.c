7b2f9a62d671d3e38d07e438eb54bad18ea501a1
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-01 09:11:14 +0000

    Print name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - the function that prints the user name
+ * @argc: The count of arguments
+ * @argv: a pointer to an array of argument(as pointer)
+ *
+ * Return: 0 for success
+ */
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
