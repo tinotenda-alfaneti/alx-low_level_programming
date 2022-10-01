f73deeeb5a44f0d21be8638d109ca8921b096a0a
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-01 09:34:57 +0000

    Print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - the main function ;-)
+ * @argc: the number of arguments passed
+ * @argv: an array of arguments
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
