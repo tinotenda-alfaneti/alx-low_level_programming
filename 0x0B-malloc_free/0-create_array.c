30a77bb0c95938437bb61644aed62243f86e0dfc
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-04 09:33:57 +0000

    Handle edge cases

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,17 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	char *ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
