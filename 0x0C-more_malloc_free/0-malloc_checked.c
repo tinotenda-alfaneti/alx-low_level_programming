876f2fefe05952233c783182b31083bfcf069e32
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-06 23:44:54 +0000

    Create README + malloc check function

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: the size of the memory
+ *
+ * Return: pointer to the memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+
+	return (ptr);
+}
