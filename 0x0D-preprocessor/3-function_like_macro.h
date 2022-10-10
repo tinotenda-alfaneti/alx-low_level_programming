aead7736b26d75afc7a455136d3d650b9e1d683d
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-10 23:07:01 +0000

    Function like macro to calculate absolute

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * ABS - calculate the absolute value
+ * Desc: function that computes absolute value
+ * @x: the value to be calculated
+ */
+
+#define ABS(x) ((x) > 0 ? (x) : (-x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
