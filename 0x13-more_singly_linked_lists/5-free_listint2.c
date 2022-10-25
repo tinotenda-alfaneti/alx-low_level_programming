abaca1f31093bd2e83a3391a2317e96efc4ee96d
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-25 23:13:34 +0000

    Free linked list using dereferencing

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*free_listint2 - frees a listint_t list
+*@head: head of a listint_t list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		prev = *head;
+		*head = (*head)->next;
+		free(prev);
+	}
+}
