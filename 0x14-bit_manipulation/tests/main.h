79a0d560f4b75d19ee2f1be7b29af1978904b01d
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-28 00:50:53 +0000

    Add tests

---
+++ 0x14-bit_manipulation/tests/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char *s);
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /* MAIN_H */
