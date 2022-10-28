ba14f1a8c94d1542f08c0c861d49bc7281fe0ec7
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-10-28 01:12:48 +0000

    Fix bug

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,23 +6,16 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int n = 0;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while (*b)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		n = n * 2 + *b - '0';
+		++b;
 	}
-
-	return (num);
+	return (n);
 }
