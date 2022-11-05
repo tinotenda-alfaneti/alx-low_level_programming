57eec98281df3b36d5380246df59017f3e5baed7
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2022-11-05 01:00:00 +0000

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,14 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int _putchar(char c);
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 #endif /* MAIN_H */
